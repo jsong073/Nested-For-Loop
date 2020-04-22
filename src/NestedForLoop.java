46f614b766052e56ee89f7ecfde5fac33764e9ba
jsong073 <jaehasong073@gmail.com>
2020-04-22 15:39:17 -0400

    first commit

---
+++ src/NestedForLoop.java
@@ -0,0 +1,10 @@
+public class NestedForLoop {
+    public static void main (String[] args) {
+        for (int row = 1; row <= 9; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+}
